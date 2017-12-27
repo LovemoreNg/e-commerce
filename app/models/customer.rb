d0a4ca9846ecb5a49322f11d2fe8a5ebbeef2a77
timmtonga <timmtonga@gmail.com>
2017-12-27 08:25:02 -0500

    Added validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :orders
+  validates_presence_of :address, :name
 end
--- db/migrate/20171220140418_create_customers.rb
+++ db/migrate/20171220140418_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers,primary_key: :customer_id do |t|
        t.string :name 
-	   t.string :address
+	      t.string :address
        t.boolean :voided, default: false 
       t.timestamps null: false
     end
